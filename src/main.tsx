0268efd26cf99e3e002e075b9ffa0839c1f777c0
Hamza Ali <askhamzaali@gmail.com>
2025-04-22 14:03:01 +0500

    router fixed

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter, Route } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
-      <Route path="/" Component={App} />
+      <Routes>
+        <Route path="/" Component={App} />
+      </Routes>
     </HashRouter>
   </StrictMode>
 );
